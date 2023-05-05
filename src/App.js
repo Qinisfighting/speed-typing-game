16233c7a4cf3f50f60a8085ebdb8b235d74f1a64
Qinisfighting <yq.qualmann@gmail.com>
2023-05-05 05:47:04 +0200

    speed typing basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+//import React, {useState, useEffect, useRef} from 'react';
+import useTypeGame from "./hooks/useTypeGame"
+
+
 
 function App() {
+
+  const {formData, inputRef, handleChange, timeIsRunning, reset, timeRemaining, numOfWords} = useTypeGame()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">  
+       <h2>How Fast Do You Type?</h2> 
+       <textarea value={formData}
+                 ref={inputRef}
+                 onChange={handleChange}
+                 disabled={!timeIsRunning}
+                 style={timeIsRunning?{backgroundColor: '#07aa59'}
+                 :{backgroundColor: 'grey',
+                   color: 'darkgray',
+                   cursor: 'not-allowed'                                                                    
+                  }}                                                                         
+                 />
+       <button onClick={() => reset()} disabled={timeIsRunning} style={timeIsRunning?{backgroundColor: 'grey'}
+                                                                                     :{color: 'lightgray'}}>
+                                                                         
+        Start</button>
+       <h4>Time remaining: {timeRemaining}</h4>
+       <h2>Word count: {numOfWords} </h2>
     </div>
   );
 }
-
 export default App;
---
+++ src/hooks/useTypeGame.js
@@ -0,0 +1,58 @@
+import {useState, useEffect, useRef} from "react"
+
+export default function useTypeGame(startingTime = 10){
+  const[formData, setFormData] = useState('')
+  const[timeRemaining, setTimeRemaining] = useState(startingTime)
+  const[timeIsRunning, setTimeIsRunning] = useState(false)
+  const [numOfWords, setNumOfWord] = useState(0)
+  const inputRef = useRef(null)
+
+  function handleChange(e) {
+    const {value} = e.target
+    setFormData(value)  
+  }
+  
+  function calWordsCount(formData){   
+    const wordsCount = formData
+                      .trim() //trim to avoid white spaces are also counted as word
+                      .split(' ')
+                      .filter(word => word !== "")  //filter to avoid empty string is also counted as 1 word
+    return  wordsCount.length  
+  }
+ 
+function reset(){
+  setTimeIsRunning(true)
+  setTimeRemaining(startingTime)
+  setFormData('')
+  setNumOfWord(0)
+  console.log(inputRef) //log out the whole textarea object unter current property
+  inputRef.current.disabled = false
+  inputRef.current.focus() 
+}
+
+  useEffect(() => {
+    // Run every 1000ms, without needing to rely on a re-render.
+    if(timeIsRunning){
+    const interval = setInterval(() => {
+      setTimeRemaining(currentTime => {
+        // If the current time is 0, then stop the interval and return 0.
+        if(currentTime <= 0){
+            clearInterval(interval);
+            setTimeIsRunning(false);
+            setNumOfWord(calWordsCount(formData))
+           
+            return 0;
+        }
+     
+        return currentTime - 1;
+    }) 
+    }, 1000);
+    // Ensure you clear the interval on dismount.
+    return () => clearInterval(interval)};
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [timeRemaining, timeIsRunning]);
+
+return {formData, inputRef, handleChange, timeIsRunning, reset, timeRemaining, numOfWords} 
+  
+} 
+  
