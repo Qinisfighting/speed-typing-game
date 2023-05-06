1a9adfb550502985cd67bd80a0c081ed51203e19
Qinisfighting <yq.qualmann@gmail.com>
2023-05-06 20:12:28 +0200

    main feathers finish, styling missing

--- src/App.js
+++ src/App.js
@@ -1,47 +1,67 @@
-import './App.css';
-import useTypeGame from "./hooks/useTypeGame"
-
-
-
+import "./App.css";
+import useTypeGame from "./hooks/useTypeGame";
 
 function App() {
+  const {
+    formData,
+    inputRef,
+    handleChange,
+    timeIsRunning,
+    reset,
+    timeRemaining,
+    randomQuote,
+    speed,
+    calAccuracy,
+  } = useTypeGame();
 
-  const {formData, inputRef, handleChange, timeIsRunning, reset, timeRemaining, randomQuote, speed, accuracy} = useTypeGame()
-  
-  
-  
   return (
-    <div className="App">  
-       <h2>Type fast and get inspired</h2>
-       <div className='quoteCard'>
-            {randomQuote? (
-              <>
-                <p className='card-text'>&quot;{randomQuote.text}&quot;</p>
-                <h5 className='card-author'> - {randomQuote.author || 'Author Unknown'} -</h5>
-                
-              </>  
-            ) : <h5>Loading...</h5>}
-        </div>  
-        
-       <textarea value={formData}
-                 placeholder={!timeIsRunning&&'Type the exact quote here as fast as you can!'}
-                 ref={inputRef}
-                 onChange={handleChange}
-                 disabled={!timeIsRunning}
-                 style={timeIsRunning?{backgroundColor: '#07aa59', color: 'rgb(26, 25, 25)',}
-                 :{backgroundColor: 'lightgrey',
-                   color: 'darkgray',
-                   cursor: 'not-allowed'                                                                    
-                  }}                                                                         
-                 />
-       <button onClick={() => reset()} disabled={timeIsRunning} style={timeIsRunning?{backgroundColor: 'grey'}
-                                                                                     :{color: 'lightgray'}}>
-                                   
-        Start</button>                                                                                     
-                                       
-       <h4>Time remaining: {timeRemaining}</h4>
-       <h2>Per Word: {speed} s </h2>
-       <h2>Accuracy: {speed} % </h2>
+    <div className="App">
+      <h2>Type fast and get inspired</h2>
+      <div className="quoteCard">
+        {randomQuote ? (
+          <>
+            <p className="card-text">&quot;{randomQuote.text}&quot;</p>
+            <h5 className="card-author">
+              {" "}
+              - {randomQuote.author || "Author Unknown"} -
+            </h5>
+          </>
+        ) : (
+          <h5>Loading...</h5>
+        )}
+      </div>
+
+      <textarea
+        value={formData}
+        placeholder={
+          timeIsRunning? '' : 'Type the exact quote here as fast as you can!'
+        }
+        ref={inputRef}
+        onChange={handleChange}
+        disabled={!timeIsRunning}
+        style={
+          timeIsRunning
+            ? { backgroundColor: "#07aa59", color: "rgb(26, 25, 25)" }
+            : {
+                backgroundColor: "lightgrey",
+                color: "darkgray",
+                cursor: "not-allowed",
+              }
+        }
+      />
+      <button
+        onClick={() => reset()}
+        disabled={timeIsRunning}
+        style={
+          timeIsRunning ? { backgroundColor: "grey" } : { color: "lightgray" }
+        }
+      >
+        Start
+      </button>
+
+      <h4>Time remaining: {timeRemaining}</h4>
+      <h2>Per Word: {formData !== "" ? `${speed} s` : ` s`}</h2>
+      <h2>Accuracy: {formData !== "" ? `${calAccuracy()} %` : ` %`} </h2>
     </div>
   );
 }
--- src/hooks/useTypeGame.js
+++ src/hooks/useTypeGame.js
@@ -1,99 +1,125 @@
-import {useState, useEffect, useRef} from "react"
+import {
+    useState,
+    useEffect,
+    useRef
+} from "react"
 
 //this file is only for practicing writing custom hook, for this app it should be ideally  put into two component files, but not made into one custom hook.
 
-export default function useTypeGame(startingTime = 10){
-  const [formData, setFormData] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(startingTime)
-  const [timeIsRunning, setTimeIsRunning] = useState(false)
-  const [quotes, setQuotes] = useState([])
-  const [randomQuote, setRandomQuote] = useState('')
-  const [speed, setSpeed] = useState('')
-  const [accuracy, setAccuracy] = useState('')
-  const inputRef = useRef(null)
+export default function useTypeGame(startingTime = 20) {
+    const [formData, setFormData] = useState('')
+    const [timeRemaining, setTimeRemaining] = useState(startingTime)
+    const [timeIsRunning, setTimeIsRunning] = useState(false)
+    const [quotes, setQuotes] = useState([])
+    const [randomQuote, setRandomQuote] = useState('')
+    const [speed, setSpeed] = useState('')
+    const inputRef = useRef(null)
 
 
-  useEffect(() => {
-    async function fetchData(){
-        const response = await fetch('https://type.fit/api/quotes')
-        const data = await response.json()
-        let randomIndex = Math.floor(Math.random() * data.length);
-    setRandomQuote(data[randomIndex])
-        setQuotes(data)
-        
-    }
-    fetchData()
-     
-}, [])
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch('https://type.fit/api/quotes')
+            const data = await response.json()
+            let randomIndex = Math.floor(Math.random() * data.length);
+            setRandomQuote(data[randomIndex])
+            setQuotes(data)
+
+        }
+        fetchData()
+
+    }, [])
 
     const getNewQuote = () => {
-    let randomIndex = Math.floor(Math.random() * quotes.length);
-    setRandomQuote(quotes[randomIndex])
+        let randomIndex = Math.floor(Math.random() * quotes.length);
+        setRandomQuote(quotes[randomIndex])
 
-}  
-  
-  function handleChange(e) {
-    const {value} = e.target
-    setFormData(value)  
-  }
-  
-  const calWordsCount = () => {   
-    const wordsCount = formData
-                      .trim() //trim to avoid white spaces are also counted as word
-                      .split(' ')
-                      .filter(word => word !== "")  //filter to avoid empty string is also counted as 1 word
-    return  wordsCount.length  
-  }
-  
- 
+    }
+
+    function handleChange(e) {
+        const {
+            value
+        } = e.target
+        setFormData(value)
+    }
+
+    const calWordsCount = () => {
+        const wordsCount = formData
+            .trim() //trim to avoid white spaces are also counted as word
+            .split(' ')
+            .filter(word => word !== "") //filter to avoid empty string is also counted as 1 word
+        return wordsCount.length
+    }
 
 
-  const calAccuracy = () => {
-    const achievedQuote = randomQuote.text
-    console.log(achievedQuote)
-//.split(' ').slice(0, calWordsCount())
-             
-return  achievedQuote
-                                                
-  }
- calAccuracy()   
+    const calAccuracy = () => {
+        const achievedQuote = String(randomQuote.text)
+            .split(' ')
+            .slice(0, calWordsCount())
+        const typedQuote = formData
+            .split(' ')
+        
 
-function reset(){
-  setTimeIsRunning(true)
-  setTimeRemaining(startingTime)
-  setFormData('')
-  setSpeed('')
-  getNewQuote()
-  console.log(inputRef) //log out the whole textarea object unter current property
-  inputRef.current.disabled = false
-  inputRef.current.focus() 
-}
-
-  useEffect(() => {
-    const wordInSecond = (startingTime/calWordsCount()).toFixed(2)
-    // Run every 1000ms, without needing to rely on a re-render.
-    if(timeIsRunning){
-    const interval = setInterval(() => {
-      setTimeRemaining(currentTime => {
-        // If the current time is 0, then stop the interval and return 0.
-        if(currentTime <= 0){
-            
-            clearInterval(interval);
-            setTimeIsRunning(false);
-            setSpeed(wordInSecond)
-           
-            return 0;
+        let correctWordsCount = 0
+        for (let i = 0; i < typedQuote.length; i++) {
+            if (achievedQuote.indexOf(typedQuote[i]) >= 0) {
+                correctWordsCount += 1
+            }  
+              
         }
-     
-        return currentTime - 1;
-    }) 
-    }, 1000);
-    // Ensure you clear the interval on dismount.
-    return () => clearInterval(interval)};
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [timeRemaining, timeIsRunning]);
+        
+        //console.log(achievedQuote)
+        //console.log(typedQuote)
+        return Math.floor(correctWordsCount / achievedQuote.length * 100)
 
-return {formData, inputRef, handleChange, timeIsRunning, reset, timeRemaining, randomQuote, speed, accuracy} 
-  
-} 
-  
+    }
+    calAccuracy()
+
+    function reset() {
+        setTimeIsRunning(true)
+        setTimeRemaining(startingTime)
+        setFormData('')
+        setSpeed('')
+        getNewQuote()
+        console.log(inputRef) //log out the whole textarea object unter current property
+        inputRef.current.disabled = false
+        inputRef.current.focus()
+    }
+
+    useEffect(() => {
+        const wordInSecond = (startingTime / calWordsCount()).toFixed(2)
+        // Run every 1000ms, without needing to rely on a re-render.
+        if (timeIsRunning) {
+            const interval = setInterval(() => {
+                setTimeRemaining(currentTime => {
+                    // If the current time is 0, then stop the interval and return 0.
+                    if (currentTime <= 0) {
+
+                        clearInterval(interval);
+                        setTimeIsRunning(false);
+                        setSpeed(wordInSecond)
+
+                        return 0;
+                    }
+
+                    return currentTime - 1;
+                })
+            }, 1000);
+            // Ensure you clear the interval on dismount.
+            return () => clearInterval(interval)
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [timeRemaining, timeIsRunning]);
+
+    return {
+        formData,
+        inputRef,
+        handleChange,
+        timeIsRunning,
+        reset,
+        timeRemaining,
+        randomQuote,
+        speed,
+        calAccuracy
+    }
+
+}
